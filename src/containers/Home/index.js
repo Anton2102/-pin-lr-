88be6a843a29ae27e48d8cf873ee1521f43b3125
Лешонков Антон <61190147+Anton2102@users.noreply.github.com>
2021-02-11 21:26:17 +0300

    Закинул набросок меню по лабам

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,25 @@
+import { Navbar, Nav, Collapse, Dropdown } from 'bootstrap-4-react';
+
 const Home = () => {
     return(
-        <>load...</>
+    <Navbar expand="lg" light bg="light">
+        <Navbar.Brand href="/">Главная</Navbar.Brand>
+        <Collapse navbar id="navbarNav">
+          <Navbar.Nav>
+            <Nav.ItemLink href="#" active>1 лаб</Nav.ItemLink>
+            <Nav.ItemLink href="#">2 лаб</Nav.ItemLink>
+            <Nav.ItemLink href="#">3 лаб</Nav.ItemLink>
+            <Nav.Item dropdown>
+              <Nav.Link dropdownToggle>Что-то ещё</Nav.Link>
+              <Dropdown.Menu>
+                <Dropdown.Item>Another</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>Another</Dropdown.Item>
+              </Dropdown.Menu>
+            </Nav.Item>
+          </Navbar.Nav>
+        </Collapse>
+      </Navbar>
     );
 }
 
