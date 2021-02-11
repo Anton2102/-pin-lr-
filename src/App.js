adc9a9323ee35edba09a4e81ddae6e2a0f60172b
Лешонков Антон <61190147+Anton2102@users.noreply.github.com>
2021-02-11 21:01:46 +0300

    connect reac-router in proj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './containers/Home/index.js'
 
-const App = () => {
-  return(
-    <div>
-      new project
-    </div>
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          {/* <Route path="/firstExample">
+            <About />
+          </Route> */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/containers/Home/index.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return(
+        <>load...</>
+    );
+}
+
+export default Home
